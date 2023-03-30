05fa76db95816f2e42a218b6687d748ae81b5e4d
Marwan Rahim <marwan820@Marwan>
2023-03-30 15:10:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3=22
+let num4= 18
+let mod = num3 % num4;
+
+let max = Math.max(1,3,20,5,6,18)
+console.log(max)
 
 // Write your code here
